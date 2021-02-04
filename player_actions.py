243700a67dddb20ad1397bded75d3aadf5438569
ronanej101 <60206562+ronanej101@users.noreply.github.com>
2021-02-04 11:42:52 -0600

    Add files via upload

---
+++ player_actions.py
@@ -0,0 +1,8 @@
+#player_actions.py
+
+#This function does x
+def check_play_again(user_input):
+    #Your code here
+
+
+check_play_again(input("Would you like to play again? Type Y for Yes or N for No: \n"))
